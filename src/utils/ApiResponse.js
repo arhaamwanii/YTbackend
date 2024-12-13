8b2f7df642ca3e1b7819f435356512d4206d4e77
arhaamwanii <arhamwani765@gmail.com>
2024-12-13 10:57:31 +0530

    utils code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,16 @@
+class ApiResponse{
+    constructor(statusCode , data , message = "Success"){
+        this.statusCode == statusCode 
+        this.data = data
+        this.message = message 
+        this.success = statusCode < 400
+    }
+}
+
+// api servers have status code
+
+// 100-199 : informational Response
+// 200-299 :  successful response 
+// 300-399 : redirection message
+// 400-499 : client error response
+// 500-599 : server error response
